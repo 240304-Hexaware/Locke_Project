04f0c504aed84e7d50a029b2663ac8342fc3dcdd
MausVII <elanlocke@gmail.com>
2024-03-10 17:54:03 -0400

    created project

---
+++ src/main/java/com/locke/babelrecords/BabelrecordsApplication.java
@@ -0,0 +1,13 @@
+package com.locke.babelrecords;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BabelrecordsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BabelrecordsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/locke/babelrecords/BabelrecordsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.locke.babelrecords;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BabelrecordsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
